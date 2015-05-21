d4a8b7665c055d7c0472e0d61d5a9edb9e799ddf
Lubomir Viluda <lubo.viluda@gmail.com>
2015-05-21 22:03:26 +0200

    1C

---
+++ Logic/Load.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TournamentManager
+{
+    public class Load
+    {
+        private string FileName;
+        public Load(string fileName)
+        {
+            FileName = fileName;
+        }
+        public List<Match> LoadMatches()
+        {
+            string buffer;
+            try
+            {
+                using (var openedFile = new StreamReader(FileName + "_matches.txt"))
+                {
+                    buffer = openedFile.ReadToEnd();
+                    openedFile.Close();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                var myNewForm = new WarningEmptyTeamAddForm("File no foud, load matches error");
+                myNewForm.ShowDialog();
+                return new List<Match>();
+            }
+            var listOfItems = JsonConvert.DeserializeObject<List<Match>>(buffer);
+            return listOfItems;
+        }
+    }
+}
---
+++ Logic/Save.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TournamentManager
+{
+    public class Save
+    {
+        public Save(string fileName, List<Match> Matches)
+        {           
+            string buffer = JsonConvert.SerializeObject(Matches);
+            using (var outfile = new StreamWriter(fileName + "_matches.txt"))
+            {
+                outfile.Write(buffer);
+                outfile.Close();
+            }
+        }
+    }
+}
